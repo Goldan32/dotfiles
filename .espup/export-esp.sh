b5657956d72f8a23466f896272392ee0cb58fffb
Daniel Arany <daniel.arany@tulip.co>
2024-07-16 15:45:50 +0200

    Add esp env setup

---
+++ .espup/export-esp.sh
@@ -0,0 +1,2 @@
+export LIBCLANG_PATH="/home/Daniel-Arany/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-17.0.1_20240419/esp-clang/lib"
+export PATH="/home/Daniel-Arany/.rustup/toolchains/esp/xtensa-esp-elf/esp-13.2.0_20230928/xtensa-esp-elf/bin:$PATH"
