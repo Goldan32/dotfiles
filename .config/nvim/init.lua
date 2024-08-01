5e07c257f52a795c80c523bdd8070a4d99c9c164
Daniel Arany <daniel.arany@tulip.co>
2024-08-01 15:37:10 +0200

    Add shellcheck and zellij config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -452,6 +452,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         bashls = {},
+        shellcheck = {},
         -- clangd = {},
         -- gopls = {},
         pyright = {},
