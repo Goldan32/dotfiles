371531f2fe5f04bb4f28ddeae928b01949c18aa5
Daniel Arany <daniel.arany@tulip.co>
2025-05-05 17:00:28 +0200

    Try to change background

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,6 +1,12 @@
 return {
   'folke/tokyonight.nvim',
+  lazy = false,
   priority = 1000, -- Make sure to load this before all the other start plugins.
+  opts = {
+    on_colors = function(colors)
+      colors.bg = '#202020'
+    end,
+  },
   init = function()
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
@@ -11,4 +17,3 @@ return {
     vim.cmd.hi 'Comment gui=none'
   end,
 }
-
