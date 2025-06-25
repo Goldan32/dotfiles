82cd91708b356b3b07101f23b86434eb4f9e87c4
Daniel Arany <daniel.arany@tulip.co>
2025-06-25 09:29:03 +0200

    Monitor config script

---
+++ .config/hypr/monitor_manager.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Only do this for work the thinkpad
+if [ "$(hostname)" != "ThinkPad" ]; then
+    exit 0
+fi
+
+# Exit if no external monitor is connected
+if ! hyprctl monitors | grep -q "DP-"; then
+    exit
+fi
+
+# Get the description of the connected monitor
+MONITOR_DESCRIPTION=$(hyprctl monitors | grep -i "description:" | cut -d ' ' -f 2-)
+
+# Logic to apply configuration based on monitor description
+if echo "$MONITOR_DESCRIPTION" | grep -q "LG Electronics LG ULTRAFINE 312MANJBDX47"; then
+    # Office 4K Monitor
+    hyprctl keyword monitor "DP-3,3840x2160@60.00,auto-right,1.25"
+elif echo "$MONITOR_DESCRIPTION" | grep -q "YOUR_2K_MONITOR_MODEL_OR_SERIAL"; then
+    # Home 2K Monitor
+    hyprctl keyword monitor "DP-3,2560x1440,auto-right,1"
+fi
