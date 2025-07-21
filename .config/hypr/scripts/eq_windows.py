52c87c5d1d6f412df7e00a365a12047172016ab7
Goldan32 <50270724+Goldan32@users.noreply.github.com>
2025-07-21 17:18:10 +0200

    Start hypr equalize

---
+++ .config/hypr/scripts/eq_windows.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sh
+import re
+
+def clean_shell(dirty):
+    return re.sub(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])', '', str(dirty))
+
+# pyright: reportAttributeAccessIssue=false
+monitors = sh.hyprctl("-j", "monitors", "show" )
+active_workspace_id = int(clean_shell(sh.jq(".[] | select(.focused == true).activeWorkspace.id",
+                                            _in = monitors)))
+
+clients = sh.hyprctl("-j", "clients")
+window_count = int(clean_shell(sh.jq("map(select(.workspace.id == 1)) | length",
+                                     _in = clients)))
+
+print(window_count)
+
+match window_count:
+    case 2:
+        pass
+    case 3:
+        pass
+    case 4:
+        pass
+    case _:
+        pass
