f5ea42a53c20b5b1ea88730ba7142ac873536d26
Goldan32 <50270724+Goldan32@users.noreply.github.com>
2025-07-22 11:18:19 +0200

    Laptopt switcher parameter

--- .config/hypr/scripts/laptop_monitor_switch.sh
+++ .config/hypr/scripts/laptop_monitor_switch.sh
@@ -5,11 +5,50 @@ mkdir -p "$HYPR_CACHE"
 
 STATE="$HYPR_CACHE/laptop_monitor_state"
 
-if [[ ! -f "$STATE" ]] || [[ "$(cat "$STATE")" == "1" ]]; then
-    hyprctl keyword monitor "eDP-1, disabled" && \
-        echo 0 > "$STATE"
-else
-    hyprctl keyword monitor "eDP-1, 1920x1200@60.00, 0x0, 1" && \
-        echo 1 > "$STATE"
+turn_off() {
+    hyprctl keyword monitor "eDP-1, disabled"
+    echo 0 > "$STATE"
+}
+
+turn_on() {
+    hyprctl keyword monitor "eDP-1, 1920x1200@60.00, 0x0, 1"
+    echo 1 > "$STATE"
+}
+
+toggle() {
+    if [[ ! -f "$STATE" ]] || [[ "$(cat "$STATE")" == "1" ]]; then
+        turn_off
+    else
+        turn_on
+    fi
+}
+
+init() {
+    turn_on
+}
+
+if [[ $# -eq 0 ]]; then
+    toggle
+    exit 0
 fi
 
+if [[ $# -ne 1 ]]; then
+    exit 1
+fi
+
+case "$1" in
+    "on")
+        turn_on
+    ;;
+    "off")
+        turn_off
+    ;;
+    "toggle")
+        toggle
+    ;;
+    *)
+        exit 1
+    ;;
+esac
+
+exit 0
