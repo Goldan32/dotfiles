3bbde9a2aa1b8be03fdf21c3e62026a2f7ce04e8
Goldan32 <50270724+Goldan32@users.noreply.github.com>
2025-10-28 17:01:32 +0100

    Add obsidian

---
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,24 @@
+return {
+  "obsidian-nvim/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  -- ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  event = {
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    "BufReadPre " .. vim.fn.expand(os.getenv("OBSIDIAN_DEFAULT_VAULT")) .. "/*.md",
+    "BufNewFile " .. vim.fn.expand(os.getenv("OBSIDIAN_DEFAULT_VAULT")) .. "/*.md",
+  },
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    workspaces = {
+      {
+        name = "udc",
+        -- path = "~/projects/udc-vault",
+        path = os.getenv("OBSIDIAN_DEFAULT_VAULT"),
+      },
+    },
+    legacy_commands = false,
+  },
+}
