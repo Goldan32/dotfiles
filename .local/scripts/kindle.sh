8dac95c0234c6f35d58e0ce765ad3db94701668c
Goldan32 <50270724+Goldan32@users.noreply.github.com>
2026-03-21 21:06:27 +0100

    Script for kindle

---
+++ .local/scripts/kindle.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+KINDLE_DIR="$HOME/.local/kindle"
+
+if [ $1 -ne 1 ]; then
+    echo "kindle.sh <attach|detach>"
+    exit 1;
+fi
+
+if [ "$1" == "attach" ]; then
+    mkdir -p "${KINDLE_DIR}"
+    jmtpfs "${KINDLE_DIR}"
+elif [ "$1" == "detach" ]; then
+    fusermount -u "${KINDLE_DIR}"
+    rmdir "${KINDLE_DIR}"
+else
+    echo "kindle.sh <attach|detach>"
+    exit 1;
+fi
+
