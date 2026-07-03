f7c49badec48fdfabe3485eb157dee8f57193743
Goldan32 <50270724+Goldan32@users.noreply.github.com>
2026-07-03 13:39:34 +0200

    Ubuntu new lsp api

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -165,12 +165,13 @@ return {
     require('mason-lspconfig').setup {
       handlers = {
         function(server_name)
-          local server = servers[server_name] or {}
+          local server, config = servers[server_name] or {}
           -- This handles overriding only values explicitly passed
           -- by the server configuration above. Useful when disabling
           -- certain features of an LSP (for example, turning off formatting for tsserver)
           server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require('lspconfig')[server_name].setup(server)
+          vim.lsp.config[server] = config
+          vim.lsp.enable(server)
         end,
       },
     }
